507536b0d93524116adff607fefeb9cb29a1e7bc
choclollipop <2924733894@qq.com>
2023-12-18 21:52:22 +0800

    [新增] 新增动态数组接口

---
+++ dynamicArray/dynamicArray.h
@@ -0,0 +1,34 @@
+#ifndef __DYNAMIC_ARRAY_H_
+#define __DYNAMIC_ARRAY_H_
+
+#define ELEMENTYP void*
+
+typedef struct dynamicArray
+{
+    ELEMENTYP data;
+    int len;
+    int capacity;
+}dynamicArray;
+
+/* 动态数组初始化 */
+int dynamicArrayInit(dynamicArray * pArray);
+
+/* 动态数组的插入操作（默认插入位置末尾） */
+int dynamicArrayInsert(dynamicArray * pArray, ELEMENTYP val);
+
+/* 动态数组指定位置插入数据 */
+int dynamicArrayInsertAppointPosData(dynamicArray * pArray, ELEMENTYP val, int pos);
+
+/* 动态数组的删除操作(默认删除末尾元素) */
+int dynamicArrayDelete(dynamicArray * pArray);
+
+/* 动态数组指定位置删除数据 */
+int dynamicArrayDeleteAppointPosData(dynamicArray * pArray, int pos);
+
+/* 动态数组的查询指定位置的数值操作 */
+int dynamicArraySearch(dynamicArray * pArray, int pos, ELEMENTYP *val);
+
+/* 修改动态数组指定位置的数值 */
+int dynamicArrayModifyAppiontPosVal(dynamicArray * pArray, ELEMENTYP val, int pos);
+
+#endif  //__DYNAMIC_ARRAY_H_
